1b3052a0d826b78efa4ebe549f13474333292579
makun <michaelmakun@aliyun.com>
2017-04-09 17:39:08 +0800

    decorate products#show

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -19,14 +19,14 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [1200, 1200]
 
   version :thumb do
     process resize_to_fill: [200, 200]
   end
 
   version :medium do
-    process resize_to_fill: [400, 400]
+    process resize_to_fill: [750, 530]
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
