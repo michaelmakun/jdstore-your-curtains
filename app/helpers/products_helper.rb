4ed8854873f38d4431944d6f2974e1829d21ac16
makun <michaelmakun@aliyun.com>
2017-04-21 01:33:15 +0800

    simply code in product#show

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -4,8 +4,16 @@ module ProductsHelper
       "productList-lastItem"
     end
   end
-  # 
+  #
   # def render_product_review_time(review)
   #   Time.zone.parse(review.created_at.to_s(:short)).in_time_zone("Beijing").to_s(:long)
   # end
+
+  def render_product_favorite(product)
+    render "products/favorite"
+  end
+
+  def render_product_add_to_cart(product)
+    render "products/add_to_cart"
+  end
 end
